8bb752fe976da18e97abde76064a841b56ad04c6
Daniel Gavrilov <dgavrilov95@gmail.com>
2015-11-12 10:55:26 +0000

    Ask for username in deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
+cd "$(dirname "$0")"
+jekyll build
+
 if [ -z ${CSUSER+x} ]; then
-  echo "CSUSER not set."
-else
-  cd "$(dirname "$0")"
-  jekyll build
-  printf "\nSyncing server... "
-  rsync -a --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r ./_site/* $CSUSER@newgate.cs.ucl.ac.uk:/cs/student/www/2015/group8/
-  printf "done.\n"
+  printf "\nEnter CS username: "
+  read CSUSER
 fi
+
+echo "Syncing server... "
+rsync -a --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r ./_site/* $CSUSER@newgate.cs.ucl.ac.uk:/cs/student/www/2015/group8/
+echo "done."
