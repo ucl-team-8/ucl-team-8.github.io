949b1fd7a403a3aee3e4456055ab1e45eea6d9d3
Daniel Gavrilov <dgavrilov95@gmail.com>
2015-12-11 00:47:41 +0000

    Rename main.js to all.js

--- assets/all.js
+++ assets/all.js
@@ -3,3 +3,5 @@
 
 {% include_relative bower_components/jquery/dist/jquery.min.js %}
 {% include_relative bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js %}
+
+{% include_relative js/main.js %}
